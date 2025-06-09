80a4e8d6ada572154dadd40050d63f917d7957ec
chagit3266 <chagit3266@gmail.com>
2025-06-10 00:22:26 +0300

    Continue building DB schemas

--- server/models/Category.model.js
+++ server/models/Category.model.js
@@ -1,7 +1,10 @@
 import { model, Schema } from "mongoose";
 
-const categorySchema =new Schema({
-
+const categorySchema = new Schema({
+    code: { type: String },
+    name: { type: String, required: true, trim: true },
+    description: { type: String },
+    
 })
 
-export default model("category",categorySchema)
+export default model("category", categorySchema)
