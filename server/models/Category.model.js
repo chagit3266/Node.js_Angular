679e9eebabf2cc4da50f3d9407704657efd6a050
chagit3266 <chagit3266@gmail.com>
2025-06-15 17:11:07 +0300

    Finish building DB schemas

--- server/models/Category.model.js
+++ server/models/Category.model.js
@@ -1,10 +1,16 @@
 import { model, Schema } from "mongoose";
 
 const categorySchema = new Schema({
-    code: { type: String },
-    name: { type: String, required: true, trim: true },
-    description: { type: String },
-    
+  code: { type: String },
+  description: { type: String },
+  recipes: [{
+    _id: { type: Schema.Types.ObjectId, ref: 'recipes' },
+    name: String
+  }],
 })
+// פונקציה שתחשב כמות מתכונים לקטגוריה 
+categorySchema.virtual('recipeCount').get(function () {
+  return this.recipes.length;
+});
 
 export default model("category", categorySchema)
--- server/models/Recipe.model.js
+++ server/models/Recipe.model.js
@@ -1,9 +1,27 @@
 import { model, Schema } from "mongoose";
 
 const recipeSchema = new Schema({
-    name: { type: String ,required: true},
-    description: {type: String},
-    category:{}
+    name: { type: String, required: true },
+    description: { type: String },
+    category: [{
+        _id: { type: Schema.Types.ObjectId, ref: 'category' },
+        description: String
+    }],
+    preparationTime: { type: Number, required: true },
+    difficultyLevel: { type: Number, min: 1, max: 5, required: true },
+    layers: [{
+        description: { type: String },
+        ingredients: [{ type: String }]
+    }],
+    prepSteps: [{ type: String }],
+    img: { type: String },
+    isPrivate: { type: Boolean, default: false },
+    owner: {
+        _id: { type: Schema.Types.ObjectId, ref: 'users' },
+        name: String
+    },
+}, {
+    timestamps: { createdAt: 'dateCreated', updatedAt: 'dateUpdated' }
 })
 
 export default model("recipe", recipeSchema)
